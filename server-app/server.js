8e54050adabf263ea7c3dcb55c0d234239c6c7ab
Jackson Jesse <jacksonjess2205@gmail.com>
2025-11-05 17:02:11 +0530

    experiment 8

--- server-app/server.js
+++ server-app/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
+
 
 const alumniRoutes = require('./routes/alumniroutes');
 
